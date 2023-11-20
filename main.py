274b548159f9e182551fcccbf1cc92de77f54943
CSComarch <ab@cd>
2023-11-20 12:24:08 +0100

    Pliki startowe

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-#dfdsfd;l
+#dfdsfd;lp[o[p
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
